f4cdbf7cd81a362ecc10339c690672471f9d43e0
Ashish Bora <ashishb@login3.maverick.tacc.utexas.edu>
2016-04-10 19:39:59 -0500

    Added  basic VQA model

---
+++ VQA_LSTM_CNN/get_data.sh
@@ -0,0 +1,6 @@
+wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/vqaRelease/train_only/data_train_val.zip
+
+wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/vqaRelease/train_only/pretrained_lstm_train_val.t7.zip
+
+unzip data_train_val.zip 
+unzip pretrained_lstm_train_val.t7.zip
---
+++ slurm/test_slurm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -A CS381V-Visual-Recogn
+#SBATCH -n 20
+#SBATCH -N 1
+#SBATCH -p gpu
+#SBATCH -t 12:00:00 
+#SBATCH -e /work/04001/ashishb/maverick/vision-project/logs/test.err
+#SBATCH -o /work/04001/ashishb/maverick/vision-project/logs/test.out
+#SBATCH -J test
+#SBATCH --mail-user=ashish.bora@utexas.edu
+#SBATCH --mail-type=all
+
+echo job $JOB_ID execution at: `date`
+NODE_HOSTNAME=`hostname -s`
+echo "running on node $NODE_HOSTNAME"
+
+# echo "Starting to tune the last layer"
+# echo -e "\n\nTraining LeNet\n\n"
+
+cd /work/04001/ashishb/maverick/vision-project/VQA_LSTM_CNN
+
+th eval.lua -input_img_h5 data_img.h5 -input_ques_h5 data_prepro.h5 -input_json data_prepro.json -model_path lstm.t7
