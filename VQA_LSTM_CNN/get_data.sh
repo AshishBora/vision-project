fa0845e58b5ca74c4c3e5759d4978e1e0037828c
AshishBora <ashish.dilip.bora@gmail.com>
2016-04-10 19:48:49 -0500

    Update get_data.sh

--- VQA_LSTM_CNN/get_data.sh
+++ VQA_LSTM_CNN/get_data.sh
@@ -4,3 +4,8 @@ wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/vqaRelease/train_only/pret
 
 unzip data_train_val.zip 
 unzip pretrained_lstm_train_val.t7.zip
+
+rm data_train_val.zip 
+rm pretrained_lstm_train_val.t7.zip
+
+th eval.lua -input_img_h5 data_img.h5 -input_ques_h5 data_prepro.h5 -input_json data_prepro.json -model_path lstm.t7
