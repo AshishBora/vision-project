edfa0a540f47d4071f4aa0f588b17c73a152f9d2
Ashish <ashish@Ashishs-MacBook-Pro.local>
2016-04-19 22:19:19 -0500

    readability

--- CaffeNet/testing.lua
+++ CaffeNet/testing.lua
@@ -4,9 +4,9 @@ require 'image';
 require 'loadcaffe';
 
 function testExample(model, input, targetValue)
-	output=model:forward(input:cuda())
-	max, indices = torch.max(output,1)
-	if(indices[1] == (targetValue+1)) then
+	output = model:forward(input:cuda())
+	max, indices = torch.max(output, 1)
+	if(indices[1] == (targetValue + 1)) then
 		return 1
 	else
 		return 0
@@ -16,25 +16,25 @@ end
 
 dofile('preproc.lua')
 dofile('string_split.lua')
--- load the model                                                             
+-- load the model
 model = torch.load('caffenet.t7')
 model:evaluate()
 model:cuda()
-
 
 list_file_path = '/work/04001/ashishb/maverick/data/listfiles/train_listfile_100.txt'
 base_path = '/work/04001/ashishb/maverick/data/'
 -- file = io.open(list_file_path, 'r')
 
-no_of_correct_examples=0
-total_no_of_examples=0
+no_of_correct_examples = 0
+total_no_of_examples = 0
+
 for line in io.lines(list_file_path) do
 
 	words = line:split(' ')
-	im_Path, label=words[1], words[2]
-	label=tonumber(label)
+	im_Path, label = words[1], words[2]
+	label = tonumber(label)
 
-	local total_path=base_path .. im_Path
+	local total_path = base_path .. im_Path
 	input = preprocess(total_path)
 	
 	no_of_correct_examples = no_of_correct_examples + testExample(model, input, label)
@@ -42,6 +42,6 @@ for line in io.lines(list_file_path) do
 	
 end
 
-print(no_of_correct_examples/total_no_of_examples)
+print(no_of_correct_examples / total_no_of_examples)
 
 
