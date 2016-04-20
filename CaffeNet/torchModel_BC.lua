ec11985950dff425e9b1b16d521b2713a921a201
Ashish <ashish@Ashishs-MacBook-Pro.local>
2016-04-20 00:53:37 -0500

    Incorporated encoder into graph, added code to test on two images

--- CaffeNet/torchModel_BC.lua
+++ CaffeNet/torchModel_BC.lua
@@ -2,19 +2,18 @@ require 'loadcaffe';
 require 'image';
 require 'cunn';
 require 'cudnn';
+require 'nngraph';
 
 dofile('preproc.lua')
 
-
-function createFullBModel(B_model)
-    local image_feat = nn.Identity()()
-    local question_input = nn.Identity()()
-    local image_output = B_model(image_feat)
-    local output = nn.DotProduct()({image_output, question_input})
-    nngraph.annotateNodes();
-    return nn.gModule({question_input, image_feat}, {output})
+function createFullBModel(getCateg)
+	local image_feat = nn.Identity()()
+	local question_input = nn.Identity()()
+	local image_output = getCateg(image_feat)
+	local output = nn.DotProduct()({image_output, question_input})
+	nngraph.annotateNodes();
+	return nn.gModule({question_input, image_feat}, {output})
 end
-
 
 
 function createCModel(input_size)
@@ -32,12 +31,15 @@ function createCModel(input_size)
 	return nngraph.gModule({image_feat1, image_feat2, question, confidence}, {y1, y2});
 end
 
-function createBCModel( B_model, C_model, encoder)
-	local image_feat1 = nn.Identity()();
-	local image_feat2 = nn.Identity()();
-	local image_feat3 = nn.Identity()();
+
+function createFullModel(B_model, C_model, encoder)
+	local image1 = nn.Identity()();
+	local image2 = nn.Identity()();
+	local image3 = nn.Identity()();
 	local question = nn.Identity()();
 
+	local image_feat1, image_feat2, image_feat3 = encoder({image1, image2, image3})
+	
 	local confidence = B_model(question, image_feat1);
 	local scores = C_model(image_feat2, image_feat3, question, confidence);
 	
@@ -46,60 +48,66 @@ function createBCModel( B_model, C_model, encoder)
 end
 
 
+function getEncoder(model)
+	-- encoder1 gets its parameters from the pretrained model
+	-- take only upto layer 21 to get 4096 dimensional
+	-- feature vector for each image
+	encoder1 = nn.Sequential()
+	for i = 1, 21 do
+		encoder1:add(model.modules[i]:clone())
+	end
 
+	-- parent module which will house the three encoders
+	-- We need three because we don't want to handle random
+	-- choice inside the network right now. We will enentually
+	-- incorporate this
+	encoder = nn.ParallelTable()
+	encoder:add(encoder1)
+	-- clone copies as well as shares the parameters when called with
+	-- extra arguments
+	encoder:add(encoder1:clone('weight','bias', 'gradWeight','gradBias')) --clone the encoder and share the weight, bias. Must also share the gradWeight and gradBias
+	encoder:add(encoder1:clone('weight','bias', 'gradWeight','gradBias')) --clone the encoder and share the weight, bias. Must also share the gradWeight and gradBias
+
+	-- put the encoder on cuda and in evalautaion mode
+	encoder:cuda()
+	encoder:evaluate()
+	return encoder
+end
+
+
+function getGetCateg(model)
+	-- GetCateg takes the encoded output given by the third encoder and
+	-- predicts the category
+	getCateg = nn.Sequential()
+	for i = 22, 24 do
+		getCateg:add(model.modules[i]:clone())
+	end
+end
 
 -- load the model
 model = torch.load('caffenet.t7')
 model:evaluate()
 model:cuda()
 
-
--- encoder1 gets its parameters from the pretraind model
--- take only upto layer 21 to get 4096 dimensional
--- feature vector for each image
-encoder1 = nn.Sequential()
-for i=1,21 do
-    encoder1:add(model.modules[i]:clone())
-end
-
--- parent module which will house the three encoders
--- We need three because we don't want to handle random
--- choice inside the network right now. We will enentually
--- incorporate this
-
-encoder = nn.ParallelTable()
-encoder:add(encoder1)
--- clone copies as well as shares the parameters when called with
--- extra arguments
-encoder:add(encoder1:clone('weight','bias', 'gradWeight','gradBias')) --clone the encoder and share the weight, bias. Must also share the gradWeight and gradBias
-encoder:add(encoder1:clone('weight','bias', 'gradWeight','gradBias')) --clone the encoder and share the weight, bias. Must also share the gradWeight and gradBias
-
--- put the encoder on cuda and in evalautaion mode
-encoder:cuda()
-encoder:evaluate()
-
--- GetCateg takes the encoded output given by the third encoder and
--- predicts the category
-getCateg = nn.Sequential()
-for i=22,24 do
-    getCateg:add(model.modules[i]:clone())
-end
-
-B = nn.ParallelTable()
-B:add(getCateg)
-B:add()
-
-
-B:cuda()
-B:evaluate()
-
--- C takes the encoded output given by the first two encoders,
--- the question (one hot for now) and a confidence value, which is B's output
-
+-- create models
+encoder = getEncoder(model)
+getCateg = getGetCateg(model)
+B_model = createFullBModel(getCateg)
+C_model = createCModel(5097)
+BC_model = createFullModel(B_model, C_model, encoder)
 
 imFolder = '../testImages/'
-path = imFolder .. 'cat.jpg'
-input = preprocess(path)
-feat = encoder1:forward(input:cuda());
-output = B:forward(feat:cuda());
+path1 = imFolder .. 'cat.jpg'
+path2 = imFolder .. 'Squirrel_posing.jpg'
+label = torch.bernoulli() + 1
+
+image_data = {}
+image_data[1] = preprocess(path[1])
+image_data[2] = preprocess(path[2])
+image_data[3] = image_data[label]:clone()
+
+ques = torch.Tensor(1000):fill(0)
+ques[285] = 1
+
+output = BC_model:forward(image_data[1],	image_data[2], image_data[3], question})
 print(output)
