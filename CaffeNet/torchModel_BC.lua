b934fef05f95012ae96f9036cb52a22c3e770f96
Ashish Bora <ashishb@login1.maverick.tacc.utexas.edu>
2016-04-21 20:03:24 -0500

    Separated model building from dummy testing

--- CaffeNet/torchModel_BC.lua
+++ CaffeNet/torchModel_BC.lua
@@ -6,7 +6,6 @@ require 'nngraph';
 
 dofile('preproc.lua')
 
-
 function createFullBModel(getCateg)
 	local image_feat = nn.Identity()()
 	local question_input = nn.Identity()()
@@ -129,34 +128,3 @@ C_model:evaluate()
 
 BC_model:cuda()
 BC_model:evaluate()
-
-
--- read and preprocess images
-imFolder = '../testImages/'
-path1 = imFolder .. 'cat.jpg'
-path2 = imFolder .. 'Squirrel_posing.jpg'
-label = torch.bernoulli() + 1
-target = 2*label-3
-
-image_data = {}
-image_data[1] = preprocess(path1)
-image_data[2] = preprocess(path2)
-image_data[3] = image_data[label]:clone()
-
--- generate a fake question
-ques = torch.Tensor(1000):fill(0)
-ques[285] = 1
-
-
--- dummy forward propagation through the model
-output = BC_model:forward({image_data[1]:cuda(), image_data[2]:cuda(), image_data[3]:cuda(), ques:cuda()})
-print(output[1][1], output[2][1])
-
--- dummy training
-crit = nn.MarginRankingCriterion(0.1)
-lr = 0.01
-
-print(target)
-for i = 1, 5 do
-	trainStep(BC_model, {image_data[1]:cuda(), image_data[2]:cuda(), image_data[3]:cuda(), ques:cuda()}, target, crit, lr)
-end
---
+++ slurm/dummy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -A CS381V-Visual-Recogn
+#SBATCH -n 20
+#SBATCH -N 1
+#SBATCH -p gpu
+#SBATCH -t 12:00:00 
+#SBATCH -e ./logs/dummy.err
+#SBATCH -o ./logs/dummy.out
+#SBATCH -J dummy
+#SBATCH --mail-user=ashish.bora@utexas.edu
+#SBATCH --mail-type=all
+
+echo job $JOB_ID execution at: `date`
+NODE_HOSTNAME=`hostname -s`
+echo "running on node $NODE_HOSTNAME"
+
+# echo "Starting to tune the last layer"
+# echo -e "\n\nTraining LeNet\n\n"
+
+cd ./CaffeNet
+
+luajit dummy.lua
